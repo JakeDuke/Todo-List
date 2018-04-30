96aa960eb56b7af9f79dcbb44b01d32891c604a7
JakeDuke <jakedukin@gmail.com>
2018-04-30 11:26:03 +0200

    initial

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-In this version we create object for our project
+In this version Gordon explains us how to store data in arrays and how to works with them.
---
+++ version1.js
@@ -0,0 +1,10 @@
+var todos = ['item 1', 'item 2', 'item 3'];
+
+console.log(todos);
+
+todos.push('new item');
+
+todos[0] = 'updated item 1';
+
+todos.splice(0,1);
+
